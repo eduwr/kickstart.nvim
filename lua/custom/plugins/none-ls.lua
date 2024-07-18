180a8d0f2f53d364a42dd7d80d97331c9d20429c
Eduardo Wronscki Ricardo <eduwric@gmail.com>
2024-07-18 20:39:42 -0300

    fix stylue package

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -5,7 +5,7 @@ return {
 
     null_ls.setup {
       sources = {
-        null_ls.builtins.formatting.stylelua,
+        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier, -- TODO: missing test
         null_ls.builtins.diagnostics.eslint_d, -- TODO: missing test
       },
