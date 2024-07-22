96177e89046671369a763aabf5cf52a4f6c656eb
Eduardo Wronscki Ricardo <eduwric@gmail.com>
2024-07-22 19:18:53 -0300

    update styles

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -11,8 +11,11 @@ return { -- You can easily change to a different colorscheme.
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'catppuccin-mocha'
+    require('catppuccin').setup {
+      transparent_background = true,
+    }
 
+    vim.cmd.colorscheme 'catppuccin-mocha'
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
   end,
